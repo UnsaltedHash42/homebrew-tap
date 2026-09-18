ebe02eb53f6458252300d32c53fd9edae3b18ff1
UnsaltedHash42 <UnsaltedHash42@users.noreply.github.com>
2026-09-17 19:28:42 -0500

    pasteback 1.0.1

--- Casks/pasteback.rb
+++ Casks/pasteback.rb
@@ -1,6 +1,6 @@
 cask "pasteback" do
-  version "1.0.0"
-  sha256 "ad67a1b6828ce139018fe94a9e72359f510326984612cc2f07a954bf1345d85f"
+  version "1.0.1"
+  sha256 "4b968df54795cec559e869bdb055d6c824168e8330393698dd577a529a41ed33"
 
   url "https://github.com/UnsaltedHash42/PasteBack/releases/download/v#{version}/Pasteback-#{version}.zip"
   name "Pasteback"
