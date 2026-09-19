18bd1164e662b5d9a0361c46665498ce28da9115
UnsaltedHash42 <UnsaltedHash42@users.noreply.github.com>
2026-09-18 22:31:33 -0500

    pasteback 1.1.0: install CLI binary as pasteback

--- Casks/pasteback.rb
+++ Casks/pasteback.rb
@@ -1,6 +1,6 @@
 cask "pasteback" do
-  version "1.0.1"
-  sha256 "4b968df54795cec559e869bdb055d6c824168e8330393698dd577a529a41ed33"
+  version "1.1.0"
+  sha256 "9a54fef92d69d261c3e648363c3281652ddc0630b09fe48ed43f1a15ffeb0f2c"
 
   url "https://github.com/UnsaltedHash42/PasteBack/releases/download/v#{version}/Pasteback-#{version}.zip"
   name "Pasteback"
@@ -10,6 +10,7 @@ cask "pasteback" do
   depends_on macos: :sonoma
 
   app "Pasteback.app"
+  binary "Pasteback.app/Contents/MacOS/pasteback-cli", target: "pasteback"
 
   zap trash: [
     "~/Library/Application Support/Pasteback",
